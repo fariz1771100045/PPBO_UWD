bc8f9abec62650ff2eb86b185747637eb05680d7
fariz1771100045 <56012959+fariz1771100045@users.noreply.github.com>
2019-10-23 10:58:45 +0700

    Update merk dan harga mobil.php

--- Merk & harga mobil.php
+++ Merk & harga mobil.php
@@ -19,19 +19,19 @@
 		 return $this->kecepatan;
 	 }
  }
- $mobil = new kendaraan("Bugatti Chiron",36 milyar,"442km/jam");
+ $mobil = new kendaraan("Bugatti Chiron","36 milyar","442km/jam");
  echo "Merk Kendaraan = ".$mobil->BacaMerk()."<br>";
  echo "Harga Kendaraan = ".$mobil->BacaHarga()."<br>";
  echo "Kecepatan Kendaraan = ".$mobil->BacaKecepatan()."<br>";
  echo "=============================================================<br><br>";
  
- $mobil2 = new kendaraan("Lamborghini Veneno",60 milyar"335km/jam");
+ $mobil2 = new kendaraan("Lamborghini Veneno","60 milyar","335km/jam");
  echo "Merk Kendaraan = ".$mobil2->BacaMerk()."<br>";
  echo "Harga Kendaraan = ".$mobil2->BacaHarga()."<br>";
  echo "Kecepatan Kendaraan = ".$mobil2->BacaKecepatan()."<br>";
  echo "=============================================================<br><br>";
  
- $mobil3 = new kendaraan("Ferrari Portofino",10 milyar"320km/jam");
+ $mobil3 = new kendaraan("Ferrari Portofino","10 milyar","320km/jam");
  echo "Merk Kendaraan = ".$mobil3->BacaMerk()."<br>";
  echo "Harga Kendaraan = ".$mobil3->BacaHarga()."<br>";
  echo "Kecepatan Kendaraan = ".$mobil3->BacaKecepatan()."<br>";
